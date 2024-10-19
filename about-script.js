975957d356d47f697b36458ab81cc23a9c1680fc
bundajongho <loviadivaaja@gmail.com>
2024-10-19 19:19:50 +0000

    Update about-script.js

--- about-script.js
+++ about-script.js
@@ -1,23 +1,31 @@
-// navbar
+// navbar 
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.navbar');
+
+hamburger.addEventListener('click', function() {
+  navMenu.classList.toggle('menu-open'); 
+
+  if (navMenu.classList.contains('menu-open')) {
+      navMenu.style.backgroundColor = "#7A4B39"; 
+  } else {
+      navMenu.style.backgroundColor = "rgba(0, 0, 0, 0)"; 
+  }
+});
+
 window.onscroll = function() {
-  scrollFunction();
+    scrollFunction();
 };
 
 function scrollFunction() {
-  var logo = document.getElementsByClassName("nav-logo")[0];
-  var navMenu = document.getElementsByClassName("navbar")[0];
+    const logo = document.getElementsByClassName("nav-logo")[0];
 
-  if (document.body.scrollTop > 450 || document.documentElement.scrollTop > 450) {
-    logo.classList.add("show");
-    navMenu.style.color = "black";
-    navMenu.style.backgroundColor = "#7A4B39";
-    navMenu.style.boxShadow = "0 1px 10px black";
-  } else {
-    logo.classList.remove("show");
-    navMenu.style.color = "white";
-    navMenu.style.backgroundColor = "rgba(0, 0, 0, 0)";
-    navMenu.style.boxShadow = "none";
-  }
+    if ((document.body.scrollTop > 450 || document.documentElement.scrollTop > 450) || navMenu.classList.contains('menu-open')) {
+        logo.classList.add("show");
+        navMenu.style.backgroundColor = "#7A4B39"; 
+        navMenu.style.boxShadow = "0 1px 10px black"; 
+    } else {
+        logo.classList.remove("show");
+        navMenu.style.backgroundColor = "rgba(0, 0, 0, 0)"; 
+        navMenu.style.boxShadow = "none"; 
+    }
 }
-
-
